abbb6e34c3d0b8c0d5bbfaa6ea4225ab9cf9e377
bamberghh <bamberghh@gmail.com>
2025-07-21 23:05:20 +0300

    Replace all tabs with 4 spaces because tabs are often inconsistently displayed

--- src/main/java/me/bamberghh/firewall/Firewall.java
+++ src/main/java/me/bamberghh/firewall/Firewall.java
@@ -12,19 +12,19 @@ import org.slf4j.LoggerFactory;
 import java.lang.reflect.InvocationTargetException;
 
 public class Firewall implements ClientModInitializer {
-	public static final String MOD_ID = "firewall";
+    public static final String MOD_ID = "firewall";
 
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final FirewallConfig CONFIG = FirewallConfig.createAndLoad(FirewallConfigModel::builderConsumer);
+    public static final FirewallConfig CONFIG = FirewallConfig.createAndLoad(FirewallConfigModel::builderConsumer);
 
-	@Override
-	public void onInitializeClient() {
+    @Override
+    public void onInitializeClient() {
         try {
             ConfigInit.init(CONFIG);
         } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
             throw new RuntimeException(e);
         }
         ConfigScreenProviders.register(MOD_ID, screen -> FirewallConfigScreen.create(CONFIG, screen));
-	}
+    }
 }
