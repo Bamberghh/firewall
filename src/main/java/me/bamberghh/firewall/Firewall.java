135e973da1f5eb8484a5aca5675f6df791b8ffe1
bamberghh <bamberghh@gmail.com>
2025-07-22 00:06:05 +0300

    Make the mod both-sided instead of client sided

--- src/main/java/me/bamberghh/firewall/Firewall.java
+++ src/main/java/me/bamberghh/firewall/Firewall.java
@@ -6,12 +6,16 @@ import me.bamberghh.firewall.config.FirewallConfig;
 import me.bamberghh.firewall.config.FirewallConfigModel;
 import me.bamberghh.firewall.config.FirewallConfigScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 
-public class Firewall implements ClientModInitializer {
+public class Firewall implements ModInitializer {
     public static final String MOD_ID = "firewall";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -19,12 +23,11 @@ public class Firewall implements ClientModInitializer {
     public static final FirewallConfig CONFIG = FirewallConfig.createAndLoad(FirewallConfigModel::builderConsumer);
 
     @Override
-    public void onInitializeClient() {
+    public void onInitialize() {
         try {
             ConfigInit.init(CONFIG);
         } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
             throw new RuntimeException(e);
         }
-        ConfigScreenProviders.register(MOD_ID, screen -> FirewallConfigScreen.create(CONFIG, screen));
     }
 }
