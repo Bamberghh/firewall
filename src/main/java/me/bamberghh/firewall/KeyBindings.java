57e8eee53c31d26541f157cf04ea7c5ea23a4c92
bamberghh <bamberghh@gmail.com>
2026-01-10 22:16:03 +0300

    Add toggling the entire mod and its logging through the config and key binds.

---
+++ src/main/java/me/bamberghh/firewall/KeyBindings.java
@@ -0,0 +1,34 @@
+package me.bamberghh.firewall;
+
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBindings {
+    public KeyBinding.Category category;
+    public KeyBinding toggleEnabled;
+    public KeyBinding toggleLoggingEnabled;
+
+    public KeyBindings() {
+        this.category = KeyBinding.Category.create(Identifier.of(Firewall.MOD_ID, "main"));
+        this.toggleEnabled = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.firewall.toggleEnabled",
+                GLFW.GLFW_KEY_UNKNOWN,
+                this.category));
+        this.toggleLoggingEnabled = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.firewall.toggleLoggingEnabled",
+                GLFW.GLFW_KEY_UNKNOWN,
+                this.category));
+    }
+
+    public void handle(MinecraftClient client) {
+        while (this.toggleEnabled.wasPressed()) {
+            Firewall.setEnabled(client, !Firewall.CONFIG.isEnabled());
+        }
+        while (this.toggleLoggingEnabled.wasPressed()) {
+            Firewall.setLoggingEnabled(client, !Firewall.CONFIG.logging.isEnabled());
+        }
+    }
+}
